d554dc54f4f13ea54777aeccee61c57d2936c6af
igihozonelly3 <igihozonelly3@gmail.com>
2026-01-27 17:37:37 +0200

    Primality test

---
+++ src/LogicBuildingProblems/EasyProblems/PrimalityTest.java
@@ -0,0 +1,26 @@
+package LogicBuildingProblems.EasyProblems;
+
+public class PrimalityTest {
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i =2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+
+
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int n = 29;
+
+        if (isPrime(n)) {
+            System.out.println(n + " is a Prime Number");
+        } else  {
+            System.out.println(n + " is Not Prime Number");
+        }
+    }
+}
